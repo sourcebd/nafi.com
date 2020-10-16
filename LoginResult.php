3147cdd05f737b621d4ead96eecdd549a8301a92
Nafi Mahmud <nativetube71@gmail.com>
2020-10-17 02:29:16 +0600

    Update LoginResult.php

--- LoginResult.php
+++ LoginResult.php
@@ -10,7 +10,9 @@ h1 {color: yellow}
 <body>
 <form action="RegistrationResult.php" method='post'>
 <h1><marquee direction=left>Login Successful! Now you can get access in our Website.</marquee></h1>
-
+<button class="btn btn-light px-5 py-2 primary-btn">
+HOME
+</button>
 </form>
 </body>
 </html>
