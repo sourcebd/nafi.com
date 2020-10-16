fef9b401e68135ee17daa8d5d708f52c5fa94657
Nafi Mahmud <nativetube71@gmail.com>
2020-10-17 02:24:44 +0600

    Add files via upload

---
+++ LoginResult.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Home Page</title>
+</head>
+<style>
+body{background-image: url('G.gif'); background-repeat:no-repeat; background-size:cover}
+h1 {color: yellow}
+</style>
+<body>
+<h1><marquee direction=left>Login Successful! Now you can get access in our Website.</marquee></h1>
+</div>
+</body>
+</html>
