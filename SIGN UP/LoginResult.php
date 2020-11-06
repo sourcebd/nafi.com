7424df2ee03473c53dc48d7c688930706cf5446a
Nafi Mahmud <nativetube71@gmail.com>
2020-11-06 19:14:37 +0600

    good
    committed

--- SIGN UP/LoginResult.php
+++ SIGN UP/LoginResult.php
@@ -15,7 +15,7 @@ header("Location: Login.php"); // Redirecting To Home Page
 </head>
 <style>
 div#PageoneBlock{width:270px;border:5px solid #ddd;padding:10px;margin:auto; text-align:center}
-body{background-image: url('G.jpg'); background-repeat:no-repeat; background-size:cover; color:orange;}
+body{background-image: url('G.jpg'); background-repeat:no-repeat; background-size:cover; color:orange;font-size:18px}
 </style>
 
 <body>
@@ -31,16 +31,17 @@ h6{color:white}
 </style>
 
 <h2 style="color:white">Welcome </h2> <h2 style="color:red"><?php echo  $_SESSION["fname"] . " " . $_SESSION["lname"]; ?><br></h2>
+<b>
 <?php
   echo "<h3>Password:</h3>";
   echo $_SESSION["password"];
 ?><br>
-<h2 style="color: #B6FF00"><marquee direction=left>Do you want to go to </marquee></h2><h2><a href="LogIndex.php" style= "color:#00E9FF; font-family:calibri">Next Page</a></h2>
+<h2 style="color: #B6FF00"><marquee direction=left>Do you want to go to </marquee></h2> <a href="LogIndex.php" style= "color:#00E9FF; font-family:calibri">Next Page</a>
 
-<h2 style="color: #B6FF00"><marquee direction=left>Do you want to</marquee> <a href="logout.php" style= "color:#00E9FF; font-family:calibri">Log Out</a></h2>
+<h2 style="color: #B6FF00"><marquee direction=left>Do you want to</marquee></h2> <a href="logout.php" style= "color:#00E9FF; font-family:calibri">Log Out</a>
 
 <h6 style="font-size:10px; font-family: verdana; text-align: center"><?php include 'Footer/footer.php';?></h6>
-
+</b>
 </div>
 </body>
 </html>
--- SIGN UP/RegistrationResult.php
+++ SIGN UP/RegistrationResult.php
@@ -14,15 +14,15 @@ header("Location: Registration.php"); // Redirecting To Home Page
 <meta name="viewport", content="width=device-width, initial-scale=1">
 </head>
 <style>
-div#PageoneBlock{width:270px;border:5px solid #ddd;padding:10px;margin:auto; text-align:center}
-body{background-image: url('G.jpg'); background-repeat:no-repeat; background-size:cover; color:orange;}
+div#RegistrationBlock{width:270px;border:5px solid #ddd;padding:10px;margin:auto; text-align:center}
+body{background-image: url('G.jpg'); background-repeat:no-repeat; background-size:cover; color:orange;font-size:18px}
 </style>
 
 <body>
 
 <h6 style="font-size:10px; font-family: verdana; color: white"><?php include 'Header/header.php';?></h6>
 
-<div id="PageoneBlock">
+<div id="RegistrationBlock">
 <h1 style="color: #00E9FF; font-family: calibri">Home</h1>
 <style>
 h3{color:lightgreen}
@@ -31,6 +31,7 @@ h6{color:white}
 </style>
 
 <h2 style="color:white">Welcome </h2> <h2 style="color:red"><?php echo  $_SESSION["fname"] . " " . $_SESSION["lname"]; ?><br></h2>
+<b>
 <?php
   echo "<h3>Password:</h3>";
   echo $_SESSION["password"];
@@ -39,12 +40,12 @@ h6{color:white}
   echo "<h3>Profession:</h3>";
   echo $_SESSION["profession"];
 ?><br>
-<h2 style="color: #B6FF00"><marquee direction=left>Do you want to go to </marquee></h2><h2><a href="RegIndex.php" style= "color:#00E9FF; font-family:calibri">Next Page</a></h2>
+<h2 style="color: #B6FF00"><marquee direction=left>Do you want to go to </marquee></h2> <a href="RegIndex.php" style= "color:#00E9FF; font-family:calibri">Next Page</a>
 
-<h2 style="color: #B6FF00"><marquee direction=left>Do you want to</marquee> <a href="Reglogout.php" style= "color:#00E9FF; font-family:calibri">Back</a></h2>
+<h2 style="color: #B6FF00"><marquee direction=left>Do you want to</marquee></h2> <a href="Reglogout.php" style= "color:#00E9FF; font-family:calibri">Back</a>
 
 <h6 style="font-size:10px; font-family: verdana; text-align: center"><?php include 'Footer/footer.php';?></h6>
-
+</b>
 </div>
 </body>
 </html>
